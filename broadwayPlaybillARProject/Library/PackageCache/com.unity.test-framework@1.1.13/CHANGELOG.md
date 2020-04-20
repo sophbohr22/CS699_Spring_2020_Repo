04e233e5b40a8026ee812a6f88c8fb8593a9bb2c
Sophie <soph.bohr22@gmail.com>
2020-04-20 14:42:16 -0500

    Added waitress target & playbills, started experimenting with SA target

---
+++ broadwayPlaybillARProject/Library/PackageCache/com.unity.test-framework@1.1.13/CHANGELOG.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5bcbd254f66ce3071bf7b4fdb8b9581a1f1b8005cec0f761921cc97b9f99c65
+size 8377
---
+++ broadwayPlaybillARProject/Library/PackageCache/com.unity.test-framework@1.1.13/UnityEditor.TestRunner/TestLaunchers/PlayerLauncher.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4cc1ecf6b4381c4114058ad42607ee63ea07f6d3f2d75d2ab9f747cf6baf64bf
+size 10059
